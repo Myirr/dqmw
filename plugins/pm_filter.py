e10dc8bb046c938314317e0563e145e2697d4eb3
Crazyboss1 <112222385+Crazyboss1@users.noreply.github.com>
2023-05-21 07:59:58 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1386,19 +1386,111 @@ async def auto_filter(client, msg, spoll=False):
     else:
         cap = f"Here is what i found for your query {search}"
     if imdb and imdb.get('poster'):
+
         try:
-            await message.reply_photo(photo=imdb.get('poster'), caption=cap[:1024],
-                                      reply_markup=InlineKeyboardMarkup(btn))
+
+            hehe = await message.reply_photo(photo=imdb.get('poster'), caption=cap[:1024], reply_markup=InlineKeyboardMarkup(btn))
+
+            try:
+
+                if settings['auto_delete']:
+
+                    await asyncio.sleep(600)
+
+                    await hehe.delete()
+
+                    await message.delete()
+
+            except KeyError:
+
+                await save_group_settings(message.chat.id, 'auto_delete', True)
+
+                await asyncio.sleep(600)
+
+                await hehe.delete()
+
+                await message.delete()
+
         except (MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty):
+
             pic = imdb.get('poster')
+
             poster = pic.replace('.jpg', "._V1_UX360.jpg")
-            await message.reply_photo(photo=poster, caption=cap[:1024], reply_markup=InlineKeyboardMarkup(btn))
+
+            hmm = await message.reply_photo(photo=poster, caption=cap[:1024], reply_markup=InlineKeyboardMarkup(btn))
+
+            try:
+
+                if settings['auto_delete']:
+
+                    await asyncio.sleep(600)
+
+                    await hmm.delete()
+
+                    await message.delete()
+
+            except KeyError:
+
+                await save_group_settings(message.chat.id, 'auto_delete', True)
+
+                await asyncio.sleep(600)
+
+                await hmm.delete()
+
+                await message.delete()
+
         except Exception as e:
+
             logger.exception(e)
-            await message.reply_text(cap, reply_markup=InlineKeyboardMarkup(btn))
+
+            fek = await message.reply_photo(photo=NOR_IMG, caption=cap, reply_markup=InlineKeyboardMarkup(btn))
+
+            try:
+
+                if settings['auto_delete']:
+
+                    await asyncio.sleep(600)
+
+                    await fek.delete()
+
+                    await message.delete()
+
+            except KeyError:
+
+                await save_group_settings(message.chat.id, 'auto_delete', True)
+
+                await asyncio.sleep(600)
+
+                await fek.delete()
+
+                await message.delete()
+
     else:
-        await message.reply_text(cap, reply_markup=InlineKeyboardMarkup(btn))
+
+        fuk = await message.reply_photo(photo=NOR_IMG, caption=cap, reply_markup=InlineKeyboardMarkup(btn))
+
+        try:
+
+            if settings['auto_delete']:
+
+                await asyncio.sleep(600)
+
+                await fuk.delete()
+
+                await message.delete()
+
+        except KeyError:
+
+            await save_group_settings(message.chat.id, 'auto_delete', True)
+
+            await asyncio.sleep(600)
+
+            await fuk.delete()
+
+            await message.delete()
+
     if spoll:
+
         await msg.message.delete()
 
 
